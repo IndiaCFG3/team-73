a861c8191b8e4038927c0f43f218435c692f93c3
Darsh <darshkpatel@gmail.com>
2020-08-15 12:10:46 +0530

    switch to env variables

--- backend/stine/settings.py
+++ backend/stine/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e#08u473b^7-87*28s!@85_%ae(77t)53-r#j2p1vw6w01v@wl'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='fallbacksecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DJANGO_DEBUG", default=True))
 
 ALLOWED_HOSTS = []
 
