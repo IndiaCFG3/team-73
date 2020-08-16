edf8cc295941d97ef5ec08b539e0fe7b83c90a63
theViz343 <vishwesh103@gmail.com>
2020-08-16 18:37:00 +0530

    created documents.py

--- backend/dashboardApp/views.py
+++ backend/dashboardApp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from.csv_parser import centre_csv_parser
 # Create your views here.
 def index(request):
     user = request.user
     group = user.groups.get()
     return HttpResponse(group)
+
+def upload_centers(request):
+    path='/datasets/Center.csv'
+    centre_csv_parser("",path)
--- backend/uploadApp/admin.py
+++ backend/uploadApp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Centre
 # Register your models here.
+admin.site.register(Centre)
