d2c1c3b6fc35e386928d7f1ab2aa5ae88a5b8dbe
theViz343 <vishwesh103@gmail.com>
2020-08-16 19:23:32 +0530

    added docs to documents.py

--- backend/uploadApp/documents.py
+++ backend/uploadApp/documents.py
@@ -1,6 +1,6 @@
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
-from .models import Centre,Employee
+from .models import Centre,Employee, Student, Course
 
 @registry.register_document
 class CentreDocument(Document):
@@ -24,4 +24,44 @@ class CentreDocument(Document):
             'state',
             'location',
         ]
-        related_models = [Employee,]
+
+
+@registry.register_document
+class EmployeeDocument(Document):
+    class Index :
+        name = 'yuvaparivartan-employee'
+
+    class Django :
+        model = Employee
+        fields = [
+            'name',
+            'designation',
+            'rating',
+            'salary',
+            'on_sabbatical',
+            'number_of_leaves',
+        ]
+
+@registry.register_document
+class StudentDocument(Document):
+    centre = fields.ObjectField(properties={
+        'name': fields.TextField(),
+        'rating': fields.IntegerField(),
+        'city' : fields.TextField(),
+        'state': fields.TextField(),
+        'location': fields.TextField(),
+    })
+    class Index :
+        name = 'yuvaparivartan-student'
+
+    class Django :
+        model = Student
+        fields = [
+            'name',
+            'dob',
+            'attendance',
+            'batch',
+            'paid_fees',
+            'passed',
+            'placed',
+        ]
