01b89c1dd60fafa25e4d43f8de4f12731f020ca3
Darsh <darshkpatel@gmail.com>
2020-08-16 19:34:49 +0530

    register models

--- backend/uploadApp/admin.py
+++ backend/uploadApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Centre, Employee
+from .models import Centre, Employee, Student, Course
 # Register your models here.
 admin.site.register(Centre)
 admin.site.register(Employee)
+admin.site.register(Student)
+admin.site.register(Course)
